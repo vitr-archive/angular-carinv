2d4ef35791be73ba425cf3ae41941080fd4b8a00
Vitaliy Ryepnoy <ryepnoy@gmail.com>
2013-05-01 00:51:02 +1000

    side search

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,11 +11,77 @@ function CarListCtrl($scope, Car) {
 
     $scope.$watch('filtered', function (newValue) {
         if (angular.isArray(newValue)) {
-            console.log(newValue.length);
+//            console.log(newValue.length);
+
             $scope.numPages = Math.ceil(newValue.length / $scope.pageSize);
             $scope.currentPage = 0;
+
+            $scope.carMakes = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['make'];
+                if(!$scope.carMakes.contains(val)) {
+                    $scope.carMakes.push(val);
+                }
+            }
+            $scope.carMakes.sort();
+
+            $scope.carModels = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['model'];
+                if(!$scope.carModels.contains(val)) {
+                    $scope.carModels.push(val);
+                }
+            }
+            $scope.carModels.sort();
+
+            $scope.carEngines = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['engine'];
+                if(!$scope.carEngines.contains(val)) {
+                    $scope.carEngines.push(val);
+                }
+            }
+            $scope.carEngines.sort();
+
+            $scope.carYears = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['year'];
+                if(!$scope.carYears.contains(val)) {
+                    $scope.carYears.push(val);
+                }
+            }
+            $scope.carYears.sort();
+
+            $scope.carTransmissions = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['transmission'];
+                if(!$scope.carTransmissions.contains(val)) {
+                    $scope.carTransmissions.push(val);
+                }
+            }
+            $scope.carTransmissions.sort();
+
+            $scope.carBodys = [];
+            for(var i = 0; i < newValue.length; i++) {
+                var val = newValue[i]['body'];
+                if (!val)
+                    continue;
+                if(!$scope.carBodys.contains(val)) {
+                    $scope.carBodys.push(val);
+                }
+            }
+            $scope.carBodys.sort();
+
         }
     }, true);
+
+
+
+    $scope.typeOptions = [
+    { name: 'Feature', value: 'feature' },
+    { name: 'Bug', value: 'bug' },
+    { name: 'Enhancement', value: 'enhancement' }
+    ];
 }
 
 
@@ -36,3 +102,11 @@ function CarDetailCtrl($scope, $routeParams, Car) {
 }
 
 //PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
+
+
+Array.prototype.contains = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === v) return true;
+    }
+    return false;
+};
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,19 +2,49 @@
 
 /* Filters */
 
-angular.module('carinvFilters', []).filter('checkmark', function() {
+var myApp = angular.module('carinvFilters', []);
+
+myApp.filter('reverse', function () {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  });
+
+myApp.filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
 });
 
+
+
+
+
+myApp.filter('searchCarBody', function() {
+    return function(input, query) {
+    if (!query)
+        return input;
+    var output = [];
+    for(var i = 0; i < input.length; i++) {
+        if (input[i].body == query)
+            output.push(input[i]);
+    }
+        return output;
+    }
+});
+
+
+
+
 //We already have a limitTo filter built-in to angular,
 //let's make a startFrom filter
 
-angular.module('carinvFilters', []).filter('startFrom', function() {
+myApp.filter('startFrom', function() {
     return function(input, start) {
         start = +start; //parse to int
         return input.slice(start);
     }
 });
+
+
 
